9b5c7f38f34345cbdd7c0e4bee1e418bc285dd78
Zyuzina Elizaveta <elizabetafox@yandex.ru>
2025-03-22 10:23:31 +0300

    final ver

--- 4-3-pwm.py
+++ 4-3-pwm.py
@@ -3,26 +3,27 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-dac = [8, 11, 7, 1, 0, 5, 12, 6]
+GPIO.setup(26, GPIO.OUT)        #GPIO пин
+GPIO.setup(22, GPIO.OUT)        #led
 
-GPIO.setup(dac, GPIO.OUT)
 
-def dec2bin(value):
-    return [int(elem) for elem in bin(value)[2:].zfill(8)]
-
-#/////шиииииииииииииииим
-GPIO.setup(12, GPIO.OUT)
-
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+p = GPIO.PWM(26, 1000) 
+q = GPIO.PWM(22, 1000) 
 p.start(0)
+q.start(0)
 try:
     while True:
         print("Enter duty cycle: ")
         dc = int(input())
         p.ChangeDutyCycle(dc)
+        q.ChangeDutyCycle(dc)
         time.sleep(0.1)
-        
-except KeyboardInterrupt:
-    pass
-p.stop()
-GPIO.cleanup()
+
+        vol = dc * (3.3 / 100)
+        print("voltage = ", vol)
+
+finally:
+    p.stop()
+    GPIO.output(26, 0)
+    GPIO.output(22, 0)
+    GPIO.cleanup()
